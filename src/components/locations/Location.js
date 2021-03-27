e0863a2fd28cffb3168f0a130a02585bf57daa15
Swarnab Garang <swarnabgarang@gmail.com>
2021-03-27 09:42:22 +0530

    styled all pages

--- src/components/locations/Location.js
+++ src/components/locations/Location.js
@@ -4,61 +4,73 @@ import RadiusFilter from "./RadiusFilter";
 import OptionFilter from "./OptionFilter";
 import axios from "axios";
 import Search from "./Search";
+import Styles from './Location.module.css';
 import { config } from "../../config";
 
 const Location = () => {
-	const [distance, setDistance] = useState(0);
-	const [option, setOption] = useState("all");
-	const [searchValue, setSearchValue] = useState("");
-	const [locations, setLocations] = useState(null);
+    const [distance, setDistance] = useState(0);
+    const [option, setOption] = useState("all");
+    const [searchValue, setSearchValue] = useState("");
+    const [locations, setLocations] = useState(null);
 
-	const location = { distance, typeofplace: option };
+    const location = { distance, typeofplace: option };
 
-	const handleChange_distance = (e) => {
-		setDistance(e.target.value);
-	};
-	const handleChange_option = (e) => {
-		setOption(e.target.value);
-	};
-	const handleSearchChange = (e) => {
-		setSearchValue(e.target.value);
-	};
+    const handleChange_distance = (e) => {
+        const flag = (e.target.value !== distance);
+        setDistance(e.target.value);
+        if (flag == true) {
+            handleFindDest();
+        }
+    };
+    const handleChange_option = (e) => {
+        const flag = (e.target.value !== option);
+        setOption(e.target.value);
+        if (flag == true) {
+            handleFindDest();
+        }
+    };
+    const handleSearchChange = (e) => {
+        const flag = (e.target.value !== searchValue);
+        setSearchValue(e.target.value);
+        if (flag == true) {
+            handleFindDest();
+        }
+    };
 
-	const handleFindDest = () => {
-		axios({
-			url: `${config.backendUrl}/travelplaces`,
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			data: location,
-		})
-			.then((res) => {
-				setLocations(res.data);
-			})
-			.catch((e) => {
-				console.log(e.message);
-			});
-	};
+    const handleFindDest = () => {
+        axios({
+            url: `${config.backendUrl}/travelplaces`,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            data: location,
+        })
+            .then((res) => {
+                setLocations(res.data);
+            })
+            .catch((e) => {
+                console.log(e.message);
+            });
+    };
 
-	return (
-		<div>
-			<div className="styles.searchHolder">
-				<Search value={searchValue} onChange={handleSearchChange} />
-			</div>
-			<section className="container">
-				<div className="styles.main">
-					<MainSection locations={locations} />
-				</div>
-				<div className="filters">
-					<h4>Filters</h4>
-					<RadiusFilter onChange={handleChange_distance} distance={distance} />
-					<OptionFilter onChange={handleChange_option} option={option} />
-				</div>
-				<button onClick={handleFindDest}> Find Destinations </button>
-			</section>
-		</div>
-	);
+    return (
+        <div className={Styles.locationContainer}>
+            <div className={Styles.searchHolder}>
+                <Search value={searchValue} onChange={handleSearchChange} />
+            </div>
+            <section className={Styles.container}>
+                <div className={Styles.main}>
+                    <MainSection locations={locations} />
+                </div>
+                <div className={Styles.filters}>
+                    <h4>Filters</h4>
+                    <RadiusFilter onChange={handleChange_distance} distance={distance} />
+                    <OptionFilter onChange={handleChange_option} option={option} />
+                </div>
+            </section>
+        </div>
+    );
 };
 
 export default Location;
